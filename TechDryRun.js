f0c683acdf7ba6ee133ddea18eeedb1406baff45
fredesse <eleegia@gmail.com>
2017-06-27 10:27:57 -0700

    Played around, waiting for a prototype

--- TechDryRun.js
+++ TechDryRun.js
@@ -1,8 +1,35 @@
 //Tech Dry Run
 
-var hrr18 = {
-  name: 'Keeley Nakamoto'
-}
+var hrr25 = {
+  names: ['Fredy  Esse', 'Anjali Sharma']
+}  
+// H////Fhreyi! I
 
-
-
+// Hi Fredy!
+//This can get messy really fast lol
+// Yeah I can see that !
+//Have you used floobits before?
+// Nope
+// My first time!
+//We used in SSP, but sometimes it was gettin g messy.
+// I think it just got messy!
+//We'll get a lot of practice fortunately
+//Yeah!
+//btw add your name to the object too
+//looks like a proper object alreaDdy #datastructures
+// :-D
+//Or maybe we can create an array of names
+//YEAH!
+// Does this look better?
+//looks better :)
+//wonder how could we do a prototype and then just create new Person
+//Yeah, that would be fun!
+//Are you going to attend the meet and greet at 10:30 ?
+// Okay, she said we are almost done
+//Yup, see you in 6 minutes I guess lol
+//:-)
+//I'll save it and push, if I have enought time I'll try to work on a prototype
+// Thanks Fredy!
+// That will be awesome !
+//ttyl :)
+// Bye :-)
